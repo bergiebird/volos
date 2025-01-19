57ad4ddb85ba24175c6769293c24dfe335e2bc24
bergie <bergiebird@gmail.com>
2025-01-19 15:00:30 -0500

    Glowing Grid Documented

---
+++ ProjectVolos/BergieBird/Working/BoardGym/board_glow.gd
@@ -0,0 +1,22 @@
+extends TileMapLayer # board_glow.gd : Controls to just make a tilemap glow, will be extended and renamed later.
+@export_category("Board Glow")
+@export var will_glow:bool  = true
+@export var will_change_color:bool  = true
+@export var will_change_transparency:bool = true
+@export var starting_color: Color = "#6a6a6a"
+@export var glow_to_color: Color = "#a84759"
+@export var color_change_speed:float=1.0
+@export var transparency_change_speed:float=1.0
+@export var transparency_minimum:float = 0.4
+@export var transparency_maximum:float = 1.0
+var time:float=0
+
+func _process(delta:float)->void:
+	if will_glow: glow_tilemap(delta)
+
+func glow_tilemap(delta:float)->void:
+	time+=delta # This needs to exist outside of if statements or else it will get choppy
+	if will_change_color:
+		modulate = starting_color.lerp(glow_to_color, (sin(time*color_change_speed)+1) / 2)
+	if will_change_transparency:
+		modulate.a = lerp(transparency_minimum, transparency_maximum ,(sin(time*transparency_change_speed)+1) / 2)
