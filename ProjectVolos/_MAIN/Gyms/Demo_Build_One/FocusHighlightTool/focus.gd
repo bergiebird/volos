d4522aff5eeb594654b306de4f55fd6a1caa623b
bergie <bergiebird@gmail.com>
2025-01-20 13:24:24 -0500

    fixed twister

--- ProjectVolos/_MAIN/Gyms/Demo_Build_One/FocusHighlightTool/focus.gd
+++ ProjectVolos/_MAIN/Gyms/Demo_Build_One/FocusHighlightTool/focus.gd
@@ -12,28 +12,7 @@ var moving:bool= false
 var tiles:int= 1
 
 func _unhandled_input(event):
-	match_action(detect_input_event(event))
-
-func move_to_next_tile(direction :Vector2)->void:
-	vfx_look_here.lifetime = 0.01
-	position += direction * tile_size
-	vfx_look_here.lifetime = 2
-
-func _on_area_entered(area :TileBasedEntity)->void:
-	area.modulate = Color.REBECCA_PURPLE
-	printt(area.name, "in focus")
-
-func _on_area_exited(area :TileBasedEntity)->void:
-	area.modulate = Color.WHITE
-	printt(area.name, "left focus")
-
-func detect_input_event(event :Input)->StringName:
-	for action in InputMap.get_actions():
-		if event.is_action_pressed(action):
-			return action
-	return ""
-
-func match_action(action):
+	var action = detect_input_event(event)
 	match action:
 		"pan_up":
 			move_to_next_tile(Vector2.UP)
@@ -49,3 +28,22 @@ func match_action(action):
 			camera.zoom = Vector2(1, 1)
 		_:
 			print("Couldn't identify the action: ", action)
+
+func move_to_next_tile(direction :Vector2)->void:
+	vfx_look_here.lifetime = 0.01
+	position += direction * tile_size
+	vfx_look_here.lifetime = 2
+
+func _on_area_entered(area :TileBasedEntity)->void:
+	area.modulate = Color.REBECCA_PURPLE
+	printt(area.name, "in focus")
+
+func _on_area_exited(area :TileBasedEntity)->void:
+	area.modulate = Color.WHITE
+	printt(area.name, "left focus")
+
+func detect_input_event(event):
+	for action in InputMap.get_actions():
+		if event.is_action_pressed(action):
+			return action
+	return null
