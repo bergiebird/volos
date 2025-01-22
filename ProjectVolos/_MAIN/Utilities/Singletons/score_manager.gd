33a4d76900c12c2707f0bfd0a15852d2288ee019
Vr00d <vroodot@gmail.com>
2025-01-22 17:48:05 -0500

    Added Score display in bottom right
    Made it kinda funky, yell at me if you hate it <3

--- ProjectVolos/_MAIN/Utilities/Singletons/score_manager.gd
+++ ProjectVolos/_MAIN/Utilities/Singletons/score_manager.gd
@@ -1,6 +1,7 @@
 extends Node #score_manager.gd
 
 @onready var score = 0
+@onready var score_label: RichTextLabel = %ScoreLabel
 
 func _ready():
 	SignalTown.who_killed_what.connect(parse_the_killing)
@@ -9,4 +10,9 @@ func parse_the_killing(responsible,target):
 	SignalTown.sfx_break.emit(target)
 	printt("parse_the_killing",responsible,target)
 	score +=1
+	adjust_score_label(score)
 	printt("score",score)
+
+
+func adjust_score_label(value: int)->void:
+	score_label.set_text(str("[hue][wave]Score: ", score, "[/wave][hue]"))
