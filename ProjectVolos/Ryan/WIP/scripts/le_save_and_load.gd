3e47dfa44d9c795a2b4ad9a3eb6dd6a9ae301843
ThisGameIsRyan <88759692+ThisGameIsRyan@users.noreply.github.com>
2025-01-22 21:00:47 -0700

    Level editor save and load
    You can't load on the web; Godot does not allow it. :(

---
+++ ProjectVolos/Ryan/WIP/scripts/le_save_and_load.gd
@@ -0,0 +1,65 @@
+extends Control
+
+@export var info_node: TextureRect
+var file_name: String = "1"
+var playable_area: Array[Vector2i]
+var parterns: Array[TileMapPattern]
+
+func _ready():
+	var x = 0
+	while x < 39:
+		var y = 0
+		while y < 17:
+			playable_area.append(Vector2i(x - 20, y - 13))
+			y += 1
+		x += 1
+
+func _save():
+	parterns.clear()
+
+	if file_name == "":
+		get_child(1).get_child(0).get_child(0).text = "Name can't be blank"
+		return
+
+	for layer in get_parent().get_parent().get_child(1).get_children():
+		parterns.append(layer.get_pattern(playable_area))
+
+	if OS.get_name() == "Web":
+		JavaScriptBridge.download_buffer(var_to_bytes_with_objects(parterns), file_name + ".lvl")
+	else:
+		var file = FileAccess.open("user://" + file_name + ".lvl", FileAccess.WRITE)
+		file.store_buffer(var_to_bytes_with_objects(parterns))
+
+func _save_as_menu():
+	get_child(1).get_child(0).get_child(0).text = "Save As"
+	get_child(1).visible = true
+
+func _save_as():
+	file_name = get_child(1).get_child(0).get_child(1).text.get_slice(".", 0)
+
+	if file_name.contains("/"):
+		get_child(1).get_child(0).get_child(0).text = "Can't contain a / "
+		return
+	elif file_name == "":
+		get_child(1).get_child(0).get_child(0).text = "Name can't be blank"
+		return
+	_save()
+	get_child(1).visible = false
+
+func _load():
+	if not FileAccess.file_exists("user://" + file_name + ".lvl"):
+		return
+
+	var file = FileAccess.open("user://" + file_name + ".lvl", FileAccess.READ) as FileAccess
+
+	var bytes = file.get_buffer(file.get_length())
+	print("Bytes loaded: ", bytes.size())
+
+	var pattern = bytes_to_var_with_objects(bytes) as Array[TileMapPattern]
+	print("Pattern created: ", pattern)
+	# print("Pattern size: ", pattern.get_size() if pattern else "null")
+
+	var layer_index = 0
+	for layer in get_parent().get_parent().get_child(1).get_children():
+		layer.set_pattern(Vector2i(-20, -13), pattern[layer_index])
+		layer_index += 1
--- ProjectVolos/Ryan/WIP/scripts/tile_map_layer_control.gd
+++ ProjectVolos/Ryan/WIP/scripts/tile_map_layer_control.gd
@@ -8,22 +8,35 @@ var mouse_right_down: bool = false
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == 1 and event.is_pressed():
-			if get_parent().get_child(0).position.y <= 64:
-				get_child(info_node.layer_index).set_cell((get_parent().get_child(0).position / 16) as Vector2i, info_node.index, info_node.coords)
+			if ui_check():
+				place_tile()
 				print((get_parent().get_child(0).position) as Vector2i)
+				#print((get_parent().get_child(0).position/16) as Vector2i)
 				mouse_left_down = true
 		elif event.button_index == 1 and not event.is_pressed():
 			mouse_left_down = false
 		elif event.button_index == 2 and event.is_pressed():
-			get_child(info_node.layer_index).erase_cell((get_parent().get_child(0).position / 16) as Vector2i)
+			remove_tile()
 			mouse_right_down = true
 		elif event.button_index == 2 and not event.is_pressed():
 			mouse_right_down = false
-
-
 	if event is InputEventMouseMotion:
 		get_parent().get_child(0).position = round((get_global_mouse_position() - Vector2(8, 8)) / 16) * 16
-		if mouse_left_down and get_parent().get_child(0).position.y <= 64:
-			get_child(info_node.layer_index).set_cell((get_parent().get_child(0).position / 16) as Vector2i, info_node.index, info_node.coords)
+		if mouse_left_down and ui_check():
+			place_tile()
 		elif mouse_right_down:
-			get_child(info_node.layer_index).erase_cell((get_parent().get_child(0).position / 16) as Vector2i)
+			remove_tile()
+	
+
+func place_tile():
+	get_child(info_node.layer_index) \
+		.set_cell((get_parent().get_child(0).position / 16) \
+			as Vector2i, info_node.index, info_node.coords)
+
+func remove_tile():
+	get_child(info_node.layer_index) \
+		.erase_cell((get_parent().get_child(0).position / 16) \
+			as Vector2i)
+
+func ui_check():
+	return get_parent().get_child(0).position.y <= 64 and get_parent().get_child(0).position.y >= -208
