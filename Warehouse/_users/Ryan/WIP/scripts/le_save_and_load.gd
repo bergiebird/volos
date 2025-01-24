a820ba71a6495313e96c898bd409187189a57cf5
ThisGameIsRyan <88759692+ThisGameIsRyan@users.noreply.github.com>
2025-01-23 23:50:49 -0700

    Upload to levels database & save and load & QOL
    You can upload any files you make to the database. Save and load locally only. Lots of QOL added to the level editor.

--- Warehouse/_users/Ryan/WIP/scripts/le_save_and_load.gd
+++ Warehouse/_users/Ryan/WIP/scripts/le_save_and_load.gd
@@ -1,11 +1,13 @@
 extends Control
 
 @export var info_node: TextureRect
-var file_name: String = "1"
+var file_name: String = "UNNAMED"
+var first_save: bool = true
 var playable_area: Array[Vector2i]
 var parterns: Array[TileMapPattern]
 
 func _ready():
+	get_parent().get_parent().get_child(1).updated_file.connect(_file_change)
 	var x = 0
 	while x < 39:
 		var y = 0
@@ -16,26 +18,49 @@ func _ready():
 
 func _save():
 	parterns.clear()
+	first_save = false
+	get_child(2).text = file_name + ".lvl"
 
-	if file_name == "":
-		get_child(1).get_child(0).get_child(0).text = "Name can't be blank"
+	if file_name == "" and first_save == false:
+		get_child(1).get_child(0).get_child(0).text = "It can't be named that"
 		return
-
+	
 	for layer in get_parent().get_parent().get_child(1).get_children():
 		parterns.append(layer.get_pattern(playable_area))
 
 	if OS.get_name() == "Web":
 		JavaScriptBridge.download_buffer(var_to_bytes_with_objects(parterns), file_name + ".lvl")
+	
+	var file = FileAccess.open("user://" + file_name + ".lvl", FileAccess.WRITE)
+	file.store_buffer(var_to_bytes_with_objects(parterns))
+
+func _save_menu():
+	if first_save:
+		get_child(1).get_child(0).get_child(0).text = "Save"
+		get_child(1).get_child(0).get_child(1).text = ""
+		get_child(1).visible = true
+		get_child(1).get_child(0).get_child(2).visible = true
+		get_child(1).get_child(0).get_child(3).visible = false
 	else:
-		var file = FileAccess.open("user://" + file_name + ".lvl", FileAccess.WRITE)
-		file.store_buffer(var_to_bytes_with_objects(parterns))
+		_save()
 
 func _save_as_menu():
 	get_child(1).get_child(0).get_child(0).text = "Save As"
+	get_child(1).get_child(0).get_child(1).text = ""
 	get_child(1).visible = true
+	get_child(1).get_child(0).get_child(2).visible = true
+	get_child(1).get_child(0).get_child(3).visible = false
+
+func _load_menu():
+	get_child(1).get_child(0).get_child(0).text = "Load"
+	get_child(1).get_child(0).get_child(1).text = ""
+	get_child(1).visible = true
+	get_child(1).get_child(0).get_child(3).visible = true
+	get_child(1).get_child(0).get_child(2).visible = false
 
 func _save_as():
-	file_name = get_child(1).get_child(0).get_child(1).text.get_slice(".", 0).to_lower()
+	file_name = get_child(1).get_child(0).get_child(1).text.get_slice(".", 0).to_upper()
+	
 
 	if file_name.contains("/"):
 		get_child(1).get_child(0).get_child(0).text = "Can't contain a / "
@@ -47,8 +72,13 @@ func _save_as():
 	get_child(1).visible = false
 
 func _load():
+	file_name = get_child(1).get_child(0).get_child(1).text.get_slice(".", 0).to_upper()
+	
 	if not FileAccess.file_exists("user://" + file_name + ".lvl"):
+		get_child(1).get_child(0).get_child(0).text = "File does not exist"
 		return
+	
+	get_child(2).text = file_name + ".lvl"
 
 	var file = FileAccess.open("user://" + file_name + ".lvl", FileAccess.READ) as FileAccess
 
@@ -63,17 +93,24 @@ func _load():
 	for layer in get_parent().get_parent().get_child(1).get_children():
 		layer.set_pattern(Vector2i(0, 0), pattern[layer_index])
 		layer_index += 1
+	get_child(1).visible = false
 
 func _upload():
-	file_name = get_child(1).get_child(0).get_child(1).text.get_slice(".", 0).to_lower()
+	file_name = get_child(1).get_child(0).get_child(1).text.get_slice(".", 0).to_upper()
 	print(Supabase.storage.from("Volos_Levels"))
 
 	if not FileAccess.file_exists("user://" + file_name + ".lvl"):
 		return
 	
-
 	var response = Supabase.storage.from("Volos_Levels").upload(file_name + ".lvl", "user://" + file_name + ".lvl")
 	if response['error'] == null:
 		print('Upload successful!')
 	else:
 		print('Upload failed: ', response['error'])
+
+func _close_menu():
+	get_child(1).visible = false
+
+func _file_change():
+	print("*" + file_name + ".lvl")
+	get_child(2).text = "*" + file_name + ".lvl"
--- Warehouse/_users/Ryan/WIP/scripts/tile_map_layer_control.gd
+++ Warehouse/_users/Ryan/WIP/scripts/tile_map_layer_control.gd
@@ -4,12 +4,17 @@ extends Node2D
 @export var info_node: TextureRect
 var mouse_left_down: bool = false
 var mouse_right_down: bool = false
+var mouse_middle_down: bool = false
+var start_pos: Vector2 = Vector2.ZERO
+var cam_start_pos: Vector2 = Vector2.ZERO
+signal updated_file
 
 func _input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == 1 and event.is_pressed():
 			if ui_check():
 				place_tile()
+				updated_file.emit()
 				print((get_parent().get_child(0).position) as Vector2i)
 				#print((get_parent().get_child(0).position/16) as Vector2i)
 				mouse_left_down = true
@@ -17,15 +22,34 @@ func _input(event):
 			mouse_left_down = false
 		elif event.button_index == 2 and event.is_pressed():
 			remove_tile()
+			updated_file.emit()
 			mouse_right_down = true
 		elif event.button_index == 2 and not event.is_pressed():
 			mouse_right_down = false
-	if event is InputEventMouseMotion:
+		elif event.button_index == 3 and event.is_pressed():
+			start_pos = get_viewport().get_mouse_position()
+			cam_start_pos = get_parent().get_child(3).position
+			mouse_middle_down = true
+		elif event.button_index == 3 and not event.is_pressed():
+			mouse_middle_down = false
+	elif event is InputEventMouseMotion:
 		get_parent().get_child(0).position = round((get_global_mouse_position() - Vector2(8, 8)) / 16) * 16
 		if mouse_left_down and ui_check():
 			place_tile()
 		elif mouse_right_down:
 			remove_tile()
+		elif mouse_middle_down:
+			pan(get_viewport().get_mouse_position())
+
+func _unhandled_input(event: InputEvent) -> void:
+	var action: StringName = detect_input_event(event)
+	var camera = get_parent().get_child(3)
+	if action == "zoom_in":
+		if camera.zoom.x * 1.1 < 43:
+			camera.zoom = camera.zoom * 1.1
+	elif action == "zoom_out":
+		if camera.zoom.x * 0.9 > 0.6:
+			camera.zoom = camera.zoom * 0.9
 	
 
 func place_tile():
@@ -39,4 +63,15 @@ func remove_tile():
 			as Vector2i)
 
 func ui_check():
-	return get_parent().get_child(0).position.y <= 408 and get_parent().get_child(0).position.y >= 12
+	var curent_pos = get_parent().get_child(0).position
+	return get_viewport().gui_get_hovered_control() == null and curent_pos.y <= 464 and curent_pos.y >= 0 and curent_pos.x <= 624 and curent_pos.x >= 0
+
+func pan(new_pos: Vector2):
+	var move_vector = new_pos - start_pos
+	get_parent().get_child(3).position = cam_start_pos - move_vector * 1 / get_parent().get_child(3).zoom.x
+	
+func detect_input_event(event: InputEvent) -> StringName:
+	for action in InputMap.get_actions():
+		if event.is_action_pressed(action):
+			return action
+	return ""
