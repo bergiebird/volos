10cbf9cee359cb60ffa4158fbe1ac5a0de4ab2ec
ThisGameIsRyan <88759692+ThisGameIsRyan@users.noreply.github.com>
2025-01-26 10:07:25 -0700

    Snapping to tiles

--- ProjectVolos/Pieces/Place-ables/kog.gd
+++ ProjectVolos/Pieces/Place-ables/kog.gd
@@ -1,23 +1,23 @@
 @icon("res://Warehouse/_users/BergieBird/Working/resources/kog.png") extends Area2D
-const CELL_SIZE :int = 16
-@export var charge_distance :int = 6
-var direction :Vector2
-var is_already_moving :bool = false
-var charging :bool = false
-@onready var north_collider :Area2D = %NorthCollider
-@onready var south_collider :Area2D = %SouthCollider
-@onready var west_collider :Area2D = %WestCollider
-@onready var east_collider :Area2D = %EastCollider
+const CELL_SIZE: int = 16
+@export var charge_distance: int = 6
+var direction: Vector2
+var is_already_moving: bool = false
+var charging: bool = false
+@onready var north_collider: Area2D = %NorthCollider
+@onready var south_collider: Area2D = %SouthCollider
+@onready var west_collider: Area2D = %WestCollider
+@onready var east_collider: Area2D = %EastCollider
 
-func _physics_process(delta :float):
+func _physics_process(delta: float):
 	if can_do(): kontrols(delta)
 
-func can_do()->bool:
+func can_do() -> bool:
 	if is_already_moving: return false
-	if charging:          return false
+	if charging: return false
 	return true
 
-func kontrols(_delta)->void:
+func kontrols(_delta) -> void:
 	if Input.is_action_just_pressed("kog_charge"):
 		process_movement(direction, true)
 	elif Input.is_action_just_pressed("kog_right"):
@@ -29,21 +29,22 @@ func kontrols(_delta)->void:
 	elif Input.is_action_just_pressed("kog_up"):
 		process_movement(Vector2.UP)
 
-func process_movement(new_direction :Vector2, is_charge :bool=false)->void:
+func process_movement(new_direction: Vector2, is_charge: bool = false) -> void:
 	if is_charge:
 		charging = true
 		Signalton.charge_started.emit()
 		for unit in charge_distance:
 			await move_once(new_direction)
 		await get_tree().create_timer(1).timeout
+		snap_to_tile()
 		charging = false
 		is_already_moving = false
 		Signalton.charge_ended.emit()
 	else:
 		move_once(new_direction)
 
-func move_once(new_direction :Vector2)->void:
-	var target_position :Vector2 = position + new_direction * CELL_SIZE
+func move_once(new_direction: Vector2) -> void:
+	var target_position: Vector2 = position + new_direction * CELL_SIZE
 	var cooldown = 0.1
 	direction = new_direction
 	if cant_move_there(target_position):
@@ -59,10 +60,13 @@ func move_once(new_direction :Vector2)->void:
 		await get_tree().create_timer(cooldown).timeout
 		is_already_moving = false
 
-func cant_move_there(target_position :Vector2):
+func cant_move_there(target_position: Vector2):
 	match direction:
-		Vector2.UP:    return north_collider.has_overlapping_bodies()
+		Vector2.UP: return north_collider.has_overlapping_bodies()
 		Vector2.RIGHT: return east_collider.has_overlapping_bodies()
-		Vector2.DOWN:  return south_collider.has_overlapping_bodies()
-		Vector2.LEFT:  return west_collider.has_overlapping_bodies()
+		Vector2.DOWN: return south_collider.has_overlapping_bodies()
+		Vector2.LEFT: return west_collider.has_overlapping_bodies()
 	return false
+
+func snap_to_tile():
+	position = round(position / 16) * 16
