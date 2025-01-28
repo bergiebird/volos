8655024c2d0162526b6c2e6a1b8bd2974e87dd7a
bergie <bergiebird@gmail.com>
2025-01-28 18:53:07 -0500

    Art and start of Gold pickup

--- ProjectVolos/Pieces/_Scripts/Containers/container_tile_entity.gd
+++ ProjectVolos/Pieces/_Scripts/Containers/container_tile_entity.gd
@@ -1,11 +1,23 @@
 @icon("res://ProjectVolos/Pieces/Breakables/Jar.png")
+@tool
 class_name Jar
 extends Area2D
 @export var cargo: PackedScene
-@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
-@onready var vfx_pot: GPUParticles2D = $VfxPot
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@export var is_crate :bool=false
+@export var is_crate_open :bool =false
+@onready var sfx: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var vfx: GPUParticles2D = $VfxPot
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 var broken :bool = false
+
+
+
+func _ready():
+	sprite.animation = get_crate()
+
+func _process(delta):
+	if Engine.is_editor_hint():
+		sprite.animation = get_crate()
 
 func drop_loot()->void:
 	if cargo:
@@ -17,8 +29,24 @@ func _on_area_entered(area: Area2D):
 	if area.name == "Kog" and area.charging:
 		if !broken:
 			%StaticBody2D.queue_free()
-			audio_stream_player_2d.play()
+			sfx.play()
 			broken = true
-			vfx_pot.emitting = true
-			animated_sprite_2d.play("Broken_Jb")
+			vfx.emitting = true
+			sprite.play(get_crate())
 			drop_loot()
+
+
+func get_crate()->String:
+	if broken:
+		if is_crate:
+			return 'cb'
+		else:
+			return 'jb'
+	else:
+		if is_crate:
+			if is_crate_open:
+				return 'co'
+			else:
+				return 'cc'
+		else:
+			return 'jw'
